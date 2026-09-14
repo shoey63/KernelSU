57c06db6ff45cd63a0b5068c3391822d80663fe8
shoey63 <shoey63@ymail.com>
2026-09-14 22:05:23 +0800

    chore: integrate SuSFS patch and Kbuild macros from CI runner

--- kernel/include/ksu.h
+++ kernel/include/ksu.h
@@ -8,11 +8,7 @@
 #define KERNEL_SU_VERSION KSU_VERSION
 
 extern struct cred *ksu_cred;
-extern bool ksu_late_loaded;
 extern bool allow_shell;
-#ifdef MODULE
-extern bool ksu_bundled;
-#endif
 extern struct selinux_policy *backup_sepolicy;
 extern bool ksu_no_custom_rc;
 
